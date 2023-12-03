6a005a775f6162da92457cd1631fda63d7cd677a
rsilvae <fitosilvae@hotmail.com>
2023-12-03 14:40:44 -0300

    Primer commit

---
+++ clase-paso-por-valor-y-paso-por-referencia.js
@@ -0,0 +1,25 @@
+let x = 1
+let y = "hola"
+let z = null
+let a = x
+let b = y
+let c = z
+console.log(x ,y ,z ,a, b ,c)
+
+a = 12
+b = "platzi"
+c = undefined
+
+console.log(x ,y ,z ,a, b ,c)
+
+//Paso por referencia 
+
+let frutas = ["manzana "]
+frutas.push("pera")
+console.log(frutas)
+
+let panes  = ["Pan tocomple"]
+let copiaDePanes = panes
+
+panes.push("pan pita")
+console.log(panes, copiaDePanes)
---
+++ paso-por-referencia.js
@@ -0,0 +1,15 @@
+// Tipo de dato complejo - Paso por referencia 
+
+let frutas = {
+    naranja: "naranjaxd"
+}
+let vegetales = frutas
+
+vegetales.naranja=  " Brocolicxd"
+
+console.log(frutas)
+let ropa = {
+    blusa: "blusa0"
+}
+ropa.pantalon= "pantalon1"
+console.log(ropa)
